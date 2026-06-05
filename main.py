97f15bd9c44c40f695c0f7288edc2b7af915e8f1
avarka0505 <d.madikhat@mail.ru>
2026-06-05 16:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from telebot import TeleBot, types
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8903906713:AAFzgXutjbqOPL2B7osbWb8YpluZJ4siiog"
 CHANNEL = "@avarka001"
 
 bot = TeleBot(BOT_TOKEN)
+
+print("Bot is starting...")
 
 def is_subscribed(user_id):
     try:
         member = bot.get_chat_member(CHANNEL, user_id)
         return member.status in ["member", "creator", "administrator"]
-    except:
+    except Exception as e:
+        print("ERROR:", e)
         return False
 
 @bot.message_handler(commands=['start'])
@@ -24,10 +27,9 @@ def start(message):
                 url=f"https://t.me/{CHANNEL.replace('@','')}"
             )
         )
-        bot.send_message(user_id, "❌ Сначала подпишись на канал!", reply_markup=markup)
+        bot.send_message(user_id, "❌ Подпишись на канал сначала!", reply_markup=markup)
         return
 
-    bot.send_message(user_id, "✅ Добро пожаловать! Доступ открыт 🌸")
+    bot.send_message(user_id, "✅ Добро пожаловать! Всё работает 🌸")
 
-print("Bot started...")
-bot.infinity_polling()
+bot.infinity_polling(skip_pending=True)
