3b7adf0e039c9a5ca4e23790ddfe0f917c95bfd0
avarka0505 <d.madikhat@mail.ru>
2026-06-05 16:05:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# проверка подписки
+from telebot import TeleBot, types
+
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHANNEL = "@avarka001"
+
+bot = TeleBot(BOT_TOKEN)
+
 def is_subscribed(user_id):
     try:
         member = bot.get_chat_member(CHANNEL, user_id)
@@ -23,13 +29,5 @@ def start(message):
 
     bot.send_message(user_id, "✅ Добро пожаловать! Доступ открыт 🌸")
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    user_id = call.message.chat.id
-
-    if is_subscribed(user_id):
-        bot.send_message(user_id, "✅ Ты подписан! Доступ открыт 🌸")
-    else:
-        bot.answer_callback_query(call.id, "❌ Ты не подписан!", show_alert=True)
-
+print("Bot started...")
 bot.infinity_polling()
